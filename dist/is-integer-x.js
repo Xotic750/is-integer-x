ec5d86f00159c88a311c89cac89f477595b56534
Graham Fairweather <xotic750@gmail.com>
2019-07-16 12:26:18 +0200

    :bookmark: v2.0.6
    Auto build

--- dist/is-integer-x.js
+++ dist/is-integer-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-15T13:06:14.086Z",
+  "date": "2019-07-16T10:26:13.843Z",
   "describe": "",
   "description": "Determine whether the passed value is an integer.",
   "file": "is-integer-x.js",
   "hash": "97f90bba988a485ead52",
   "license": "MIT",
-  "version": "2.0.5"
+  "version": "2.0.6"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
