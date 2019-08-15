1fa76b2d15bca2ffcdb449e7bebd752a393bd683
Graham Fairweather <xotic750@gmail.com>
2019-08-15 20:38:20 +0200

    Maintenance

--- dist/is-integer-x.js
+++ dist/is-integer-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-15T17:47:17.674Z",
+  "date": "2019-08-15T18:38:14.439Z",
   "describe": "",
   "description": "Determine whether the passed value is an integer.",
   "file": "is-integer-x.js",
